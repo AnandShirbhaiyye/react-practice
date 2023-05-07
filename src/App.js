170e8cae19a6774177a4b4d3e627f545abcc44ea
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-07 23:26:03 +0530

    props practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import Chocolate from './Chocolate';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-center'>Hello Anand...{10+10}</h1>
+      <h1 className='app-header'>Chocolate App 💙</h1>
+
+      <Chocolate title="Kharbuj" description="Khabuj is fruit"/>
+      <Chocolate title="Dairy Milk" description="dairy milk is sweet"/>
+      <Chocolate title="Perk" description="perk is chocolate"/>
+      
     </div>
   );
 }

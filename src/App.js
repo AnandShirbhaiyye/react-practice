c89f32cf2a5da9911e56244699d77a32be321ab7
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-07 23:28:14 +0530

    props practice

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <h1 className='text-center'>Hello Anand...{10+10}</h1> <hr/>
     <div className="App">
-      <h1 className='text-center'>Hello Anand...{10+10}</h1>
-      <h1 className='app-header'>Chocolate App 💙</h1>
-
+      <h1 className='app-header text-center mt-3'>Chocolate App 💙</h1>
       <Chocolate title="Kharbuj" description="Khabuj is fruit"/>
       <Chocolate title="Dairy Milk" description="dairy milk is sweet"/>
       <Chocolate title="Perk" description="perk is chocolate"/>
       
     </div>
+    </>
   );
 }
 

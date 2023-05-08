36f3ea1aa91ab7f8b0ee5d215e70b9b79b0b796b
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-08 23:12:47 +0530

    added css in State.css

--- src/components/State/State.js
+++ src/components/State/State.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
+import './State.css'
 
 export default function State() {
     const [count, setCount] = useState(0);
   return (
     <>
+    <div className='usestate-container'>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
     </>
   )
 }

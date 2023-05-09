25e462c1fa6b9181e4e8ce15df58809fbcfc110c
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-10 01:08:12 +0530

    create props folder and added files and some css changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import Chocolate from './Chocolate';
 import './App.css';
+import Props from './components/Props/Props';
 import State from './components/State/State';
 
 function App() {
   return (
     <>
     <h1 className='text-center'>Hello Anand...{10+10}</h1> <hr/>
-    <div className="App">
-      <h1 className='app-header text-center mt-3'>Chocolate App 💙</h1>
-      <Chocolate title="Kharbuj" description="Khabuj is fruit"/>
-      <Chocolate title="Dairy Milk" description="dairy milk is sweet"/>
-      <Chocolate title="Perk" description="perk is chocolate"/>
-      <Chocolate title="Anand" description="Anand is a boy"/>
-    </div>
+    <Props/>
     <State/>
     </>
   );
---
+++ src/components/Props/Props.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Chocolate from './Chocolate'
+
+export default function Props() {
+  return (
+    <>
+    <div className="container">
+        <div className='row'>
+    <h1 className='app-header text-center mt-3'>Chocolate App 💙</h1>
+    <Chocolate title="Kharbuj" description="Khabuj is fruit"/>
+    <Chocolate title="Dairy Milk" description="dairy milk is sweet"/>
+    <Chocolate title="Perk" description="perk is chocolate"/>
+    <Chocolate title="Anand" description="Anand is a boy"/>
+  </div>
+  </div>
+  </>
+  )
+}

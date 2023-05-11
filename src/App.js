18fc12e83de44074d3321d2cb7ef54ff2b0778f4
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-12 00:09:20 +0530

    feat: added react router dom and path in components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Props from './components/Props/Props';
 import State from './components/State/State';
+import Map from "./components/Map/Map";
+
+import './App.css';
 
 function App() {
   return (
     <>
     <h1 className='text-center'>Hello Anand...{10+10}</h1> <hr/>
-    <Props/>
-    <State/>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Props/>} />
+        <Route path="/state" element={<State/>} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }

dd4707601d1a18a52a0cf69d90d7015e4425e3e9
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-15 23:35:53 +0530

    create form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 export default function Form() {
   return (
-    <div>Form</div>
+    <>
+    <form>
+      <label>
+        FullName:
+        <input type="text"/>
+      </label>
+      <label>
+        Email:
+        <input type="email"/>
+      </label>
+      <label>
+        Mobile-Number:
+        <input type="phone"/>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    </>
   )
 }

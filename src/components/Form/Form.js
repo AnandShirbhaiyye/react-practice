761227a6a8a8ed49e9bdc767600674d0508ca8ef
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-17 12:10:11 +0530

    create basic design of form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React from "react";
+import "./Form.css";
 
 export default function Form() {
   return (
     <>
-    <form>
-        <input type="text" placeholder='enter your fullname' className='mt-2'/><br/>
+      <div className="container">
+        <div className="row">
+        <div className="form-container text-center">
+          <form>
+            <input
+              type="text"
+              placeholder="enter your fullname"
+              className="mt-2"
+            />
+            <br />
 
-        <input type="email" placeholder='enter your email' className='mt-2'/><br/>
-  
-        <input type="phone" placeholder='enter your phone number' className='mt-2' /><br/>
+            <input
+              type="email"
+              placeholder="enter your email"
+              className="mt-2"
+            />
+            <br />
 
-      <button type="submit" className='mt-2'>Submit</button>
-    </form>
+            <input
+              type="phone"
+              placeholder="enter your phone number"
+              className="mt-2"
+            />
+            <br />
+
+            <button type="submit" className="mt-2">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      </div>
     </>
-  )
+  );
 }

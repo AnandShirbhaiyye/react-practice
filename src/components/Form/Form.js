46b7998636ec845633ed38424a53bf641ff69950
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-30 22:20:27 +0530

    added box shadow and change button name

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -59,7 +59,7 @@ export default function Form() {
               <br />
 
               <div class="d-grid gap-2 col-6 mx-auto mt-2">
-              <button class="btn btn-primary" type="submit">Button</button>
+              <button class="btn btn-primary" type="submit">Submit</button>
               </div>
             </form>
           </div>

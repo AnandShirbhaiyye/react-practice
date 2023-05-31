723578731c6533d90e540efce7d140f61435173d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-05-31 23:38:41 +0530

    create practice of usestate

--- src/components/State/State.js
+++ src/components/State/State.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import { useState } from 'react';
-import './State.css'
+import React from "react";
+import { useState } from "react";
+import "./State.css";
 
 export default function State() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   return (
     <>
-    <div className='usestate-container text-center'>
-      <div className='text-center'>
-      <h1>Count: {count}</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="usestate-container text-center">
+              <div className="text-center">
+                <h1>Count: {count}</h1>
+              </div>
+              <button
+                className="btn btn-warning me-5"
+                onClick={() => setCount(count + 1)}
+              >
+                Increment
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => setCount(count - 1)}
+              >
+                Decrement
+              </button>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="usestate-container text-center">
+              <div className="text-center">
+                <h1>Count: {count}</h1>
+              </div>
+              <button
+                className="btn btn-warning me-5"
+                onClick={() => setCount(count + 1)}
+              >
+                Increment
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => setCount(count - 1)}
+              >
+                Decrement
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <button className='btn btn-warning me-5' onClick={() => setCount(count + 1)}>Increment</button>
-      <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Decrement</button>
-    </div>
     </>
-  )
+  );
 }

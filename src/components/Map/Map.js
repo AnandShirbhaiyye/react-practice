ed75c64a832c96c0077c60f5d2d8320aac3b3862
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-02 18:13:37 +0530

    change background color and add container

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,10 +5,12 @@ export default function Map() {
   const students = ["Anand", "Kajal", "Kanchan", "Ujwal"];
   return (
     <>
-      <h1 className="text-center">Map</h1>
+      <h1 className="text-center">Map Function</h1>
+      <div className="container mt-4">
       {students.map((stud) => {
         return <Student name={stud} />;
       })}
+      </div>
     </>
   );
 }

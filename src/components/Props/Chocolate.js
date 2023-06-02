19d8da49199d238c6e8458e82585bbfb7be104de
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-02 18:07:10 +0530

    format documemt

--- src/components/Props/Chocolate.js
+++ src/components/Props/Chocolate.js
@@ -1,12 +1,12 @@
-import './Chocolate.css';
+import "./Chocolate.css";
 
-function Chocolate({title, description}) {
+function Chocolate({ title, description }) {
   return (
     <div className="chocolate-card">
-        <h2>{title}</h2>
-        <p>{description}</p>
+      <h2>{title}</h2>
+      <p>{description}</p>
     </div>
-  )
+  );
 }
 
-export default Chocolate
+export default Chocolate;

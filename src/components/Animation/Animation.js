f8750f739e34d9fdc50e2494cc54984782ed4aaf
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-05 23:57:02 +0530

    added css file

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -4,12 +4,12 @@ import "./Animation.css";
 export default function Animation() {
   return (
     <>
-      <h1>Car Run Animation</h1>
+      <h1 className="text-center animation-car">Car Running Animation</h1>
 
       <div class="car-container">
         <img
           class="img"
-          src=""
+          src="https://cdn.lowgif.com/full/8f5a48839e61c7c7-race-car-animated-free-download-best-race-car-animated.gif"
           alt=""
         />
       </div>
